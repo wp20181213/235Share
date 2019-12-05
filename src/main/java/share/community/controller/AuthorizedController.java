407898ba090fafd9dc9ce828ed02f8a1ca4d0c8b
Wp <490125804@qq.com>
2019-12-06 00:00:15 +0800

    增加登录跳转回主页，登陆成功右上角显示用户名

--- src/main/java/share/community/controller/AuthorizedController.java
+++ src/main/java/share/community/controller/AuthorizedController.java
@@ -1,5 +1,7 @@
 package share.community.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
@@ -27,7 +29,8 @@ public class AuthorizedController {
 
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code") String code,
-                           @RequestParam(name = "state") String state) {
+                           @RequestParam(name = "state") String state,
+                           HttpServletRequest request) {
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setClient_id(clientId);
         accessTokenDTO.setClient_secret(clientSecret);
@@ -36,7 +39,14 @@ public class AuthorizedController {
         accessTokenDTO.setState(state);
         String accessToken = gitHubProvider.getAccessoken(accessTokenDTO);
         GitHubUser user = gitHubProvider.getUser(accessToken);
-        System.out.println(user.getName());
-        return "index";
+        if(user != null){
+            //登录成功:写cookie和session
+            request.getSession().setAttribute("user", user);
+            return "redirect:/";
+        }
+        else{
+            //登录失败：重新登陆
+            return "redirect:/";
+        }
     }
 }
