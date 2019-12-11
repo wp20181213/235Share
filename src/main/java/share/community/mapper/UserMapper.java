2e76336deeaecc8dc2730d6a2f2878cb20467e59
Wp <490125804@qq.com>
2019-12-11 13:01:07 +0800

    修改插入数据库的操作

--- src/main/java/share/community/mapper/UserMapper.java
+++ src/main/java/share/community/mapper/UserMapper.java
@@ -2,12 +2,10 @@ package share.community.mapper;
 
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Select;
-
 import share.community.model.User;
 
 @Mapper
 public interface UserMapper {
-    @Insert("insert into user (name, accountId, Token, gmtCreateTime, gmtModifiedTime) values (#{name},#{accountId}, #{Token}, #{gmtCreateTime}, #{gmtModifiedTime},)")
+    @Insert("insert into user (name,account_id,token,gmt_create,gmt_modified) values (#{name},#{account_id},#{token},#{gmt_create},#{gmt_modified})")
     void insert(User user);
 }
