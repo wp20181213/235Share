be76f9293b7c6f7ab093346ab54a97a172adf2f4
WangPeng <490125804@qq.com>
2019-12-23 21:00:04 +0800

    完成发布功能

--- src/main/java/share/community/mapper/QuestionMapper.java
+++ src/main/java/share/community/mapper/QuestionMapper.java
@@ -1,9 +1,12 @@
 package share.community.mapper;
 
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import share.community.model.Question;
 
 @Mapper
-public class QuestionMapper {
+public interface QuestionMapper {
 
-
+    @Insert("insert into question(title, description, gmt_create, gmt_modified, creator, tag) values(#{title}, #{description}, #{gmtCreate}, #{gmtModified}, #{creator}, #{tag})")
+    void create(Question question);
 }
