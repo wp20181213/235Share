78460150989f5fa543dd4bffc9f438b864d5444e
WangPeng <490125804@qq.com>
2019-12-23 20:59:47 +0800

    创建发布问题类

---
+++ src/main/java/share/community/model/Question.java
@@ -0,0 +1,99 @@
+package share.community.model;
+
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+
+import javax.swing.*;
+
+public class Question {
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle(String title) {
+        return this.title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription(String description) {
+        return this.description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getTag(String tag) {
+        return this.tag;
+    }
+
+    public void setTag(String tag) {
+        this.tag = tag;
+    }
+
+    public Long getGmtCreate() {
+        return gmtCreate;
+    }
+
+    public void setGmtCreate(Long gmtCreate) {
+        this.gmtCreate = gmtCreate;
+    }
+
+    public Long getGmtModified() {
+        return gmtModified;
+    }
+
+    public void setGmtModified(Long gmtModified) {
+        this.gmtModified = gmtModified;
+    }
+
+    public Integer getCreator() {
+        return Creator;
+    }
+
+    public void setCreator(Integer creator) {
+        Creator = creator;
+    }
+
+    public Integer getViewCount() {
+        return viewCount;
+    }
+
+    public void setViewCount(Integer viewCount) {
+        this.viewCount = viewCount;
+    }
+
+    public Integer getCommentCount() {
+        return commentCount;
+    }
+
+    public void setCommentCount(Integer commentCount) {
+        this.commentCount = commentCount;
+    }
+
+    public Integer getLikeCount() {
+        return likeCount;
+    }
+
+    public void setLikeCount(Integer likeCount) {
+        this.likeCount = likeCount;
+    }
+
+    private Integer id;
+    private String title;
+    private String description;
+    private String tag;
+    private Long gmtCreate;
+    private Long gmtModified;
+    private Integer Creator;
+    private Integer viewCount;
+    private Integer commentCount;
+    private Integer likeCount;
+}
