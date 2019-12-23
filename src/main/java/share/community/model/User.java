9680428b27e41800fbb8cc06e8297c6e27caab47
WangPeng <490125804@qq.com>
2019-12-23 21:14:44 +0800

    构造函数改为@Data

--- src/main/java/share/community/model/User.java
+++ src/main/java/share/community/model/User.java
@@ -1,63 +1,14 @@
 package share.community.model;
 
+import lombok.Data;
+
+@Data
 public class User {
     private Integer userId;
     private String name;
-
-    public String getAccount_id() {
-        return account_id;
-    }
-
-    public void setAccount_id(String account_id) {
-        this.account_id = account_id;
-    }
-
     private String account_id;
-
-    public Integer getUserId() {
-        return userId;
-    }
-
-    public void setUserId(Integer userId) {
-        this.userId = userId;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getToken() {
-        return token;
-    }
-
-    public void setToken(String token) {
-        this.token = token;
-    }
-
-
     private String token;
-
-    public Long getGmt_create() {
-        return gmt_create;
-    }
-
-    public void setGmt_create(Long gmt_create) {
-        this.gmt_create = gmt_create;
-    }
-
     private Long gmt_create;
-
-    public Long getGmt_modified() {
-        return gmt_modified;
-    }
-
-    public void setGmt_modified(Long gmt_modified) {
-        this.gmt_modified = gmt_modified;
-    }
-
     private Long gmt_modified;
+    private String avatarUrl;
 }
