9390dd12b639fdb163fe9cb65269f97c4c1d6bef
WangPeng <490125804@qq.com>
2019-12-23 21:19:56 +0800

    用户登录获取头像

--- src/main/java/share/community/controller/AuthorizedController.java
+++ src/main/java/share/community/controller/AuthorizedController.java
@@ -52,6 +52,7 @@ public class AuthorizedController {
             user.setAccount_id(String.valueOf(githubUser.getId()));
             user.setGmt_create(System.currentTimeMillis());
             user.setGmt_modified(user.getGmt_create());
+            user.setavatar_Url(githubUser.getAvatar_Url());
             userMapper.insert(user);
             response.addCookie(new Cookie("token", token));
 //            request.getSession().setAttribute("user", githubUser);
