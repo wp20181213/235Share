ad6dce58c644002a5231d808fb7921b0e073f72c
WangPeng <490125804@qq.com>
2019-12-28 17:54:21 +0800

    增加读取数据库question表方法

--- src/main/java/share/community/mapper/UserMapper.java
+++ src/main/java/share/community/mapper/UserMapper.java
@@ -5,9 +5,13 @@ import share.community.model.User;
 
 @Mapper
 public interface UserMapper {
+
     @Insert("insert into user (name,account_id,token,gmt_create,gmt_modified, avatar_Url) values (#{name},#{account_id},#{token},#{gmt_create},#{gmt_modified},#{avatarUrl})")
     void insert(User user);
     
     @Select("select * from user where token = #{token}")
     User findByToken(@Param("token") String token);
+
+    @Select("select * from user where id = #{id}")
+    User findById(@Param("id") Integer id);
 }
